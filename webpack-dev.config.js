c85c73db220c443c7ec78484ba60cdf535889bf4
passtech <contact@pass-tech.fr>
2026-05-16 21:32:15 +0200

    New webpack

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -18,7 +18,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.ts$/,
-                loader: 'ts-loader'
+                loader: 'awesome-typescript-loader'
             }
         ]
     }
